b7f7a380ae565bc3680b75ee1e865b3f09a18ef4
azzyjk <azzyjk@naver.com>
2019-12-11 14:48:52 +0000

    Change code because memory over

--- Sort/3.cpp
+++ Sort/3.cpp
@@ -1,36 +1,25 @@
 #include<iostream>
-#define MAX 10000
+#include<cstdio>
 
 using namespace std;
 
-void set(int *ar){
-	for(int i=0; i<MAX-1; i++){
-		ar[i+1]+=ar[i];
-	}
-}
-
-void print(int ar[], int N){
-	for(int i=0; i<N; i++){
-		cout<<ar[i]<<endl;
-	}
-}
-
 int main(){
-	int N, num, ar[MAX]={'\0',};
+	int N, num, ar[10001]={'\0',};
 	cin>>N;
-	int res[N], raw[N];
 
 	for(int i=0; i<N; i++){
-		cin>>num;
-		raw[i] = num;
+		scanf("%d", &num);
 		ar[num]++;
 	}
-	set(ar);
-	for(int i=0; i<N; i++){
-		res[ar[raw[i]]-1]=raw[i];
-		ar[raw[i]]--;
+
+	for(int i=0; i<=10000; i++){
+		if(ar[i]==0) continue;
+		else {
+			while(ar[i]!=0){
+				printf("%d\n", i);
+				ar[i]--;	
+			}
+		}
 	}
-	print(res, N);
-	
-	
+	return 0;
 }
