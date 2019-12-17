b0633bce55c4be97b3e8b17c7f4c4f01255fb0c0
azzyjk <azzyjk@naver.com>
2019-12-17 14:16:53 +0000

    Change code because time over

--- Sort/8.cpp
+++ Sort/8.cpp
@@ -1,47 +1,24 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
-#include<utility>
+#include<set>
 #include<cstring>
-#include<cstdio>
+#include<utility>
 
 using namespace std;
 
-bool cmp(const pair<string, int> a, const pair<string, int> b){
-	int i=0;
-	if(a.second<b.second) return true;
-	else if(a.second==b.second){
-		while(a.first[i]==b.first[i]){
-			i++;
-		}
-		return a.first[i]<b.first[i];
-	}
-	else return false;
-}
-
-bool check(vector<pair<string,int> > v, string str){
-	for(vector<int>::size_type i=0; i<v.size(); i++){
-		if(v[i].first==str) return false;
-	}
-	return true;
-}
-
 int main(){
-	int N, num;
+	int N;
 	string str;
-	vector<pair<string, int> > v;
+	set<pair<int, string> > s;
 	
-	scanf("%d", &N);
+	cin>>N;
 	
 	for(int i=0; i<N; i++){
 		cin>>str;
-		if(check(v, str)) v.push_back(make_pair(str,str.length()));
+		s.insert(make_pair(str.length(), str));
 	}
 	
-	sort(v.begin(), v.end(), cmp);
-	
-	for(vector<string>::size_type i=0; i<v.size(); i++){
-		printf("%s\n", v[i].first.c_str());
+	for(set<pair<int, string> >::iterator iter=s.begin(); iter != s.end(); iter++){
+		cout<<(*iter).second<<endl;
 	}
 	
 	return 0;
