57bd7a790750b841027a078fdbc7c1c204539723
azzyjk <azzyjk@naver.com>
2020-01-06 14:06:00 +0000

    Fix the problem that output wrong

--- Stack/4.cpp
+++ Stack/4.cpp
@@ -8,20 +8,38 @@ int main(){
 	cin.tie(0);
 	cout.tie(0);
 	
-	stack<char> s;
 	string str;
+	stack<char> s;
 	int i;
-	while(getline(cin, str)){
+	bool check;
+	while(1){
+		check = true;
+		getline(cin, str);
 		if(str.length()==1&&str[0]=='.') break;
+		
 		for(i=0; str[i]!='.'; i++){
 			if(str[i]=='('||str[i]=='[') s.push(str[i]);
-			else if(!s.empty()&&((str[i]==')'&&s.top()=='(')||(str[i]==']'&&s.top()=='['))) s.pop();
-		}
-		if(s.empty()&&str[i]=='.') cout<<"yes"<<endl;
-			else {
-				while(!s.empty()) s.pop();
-				cout<<"no"<<endl;
+			else if(str[i]==')'){
+				if(!s.empty()&&s.top()=='(') s.pop();	
+				else{
+					check=false;
+					break;
+				}
 			}
+			else if(str[i]==']'){
+				if(!s.empty()&&s.top()=='[') s.pop();
+				else{
+					check=false;
+					break;
+				}
+			}
+		}
+		
+		if(s.empty()&&str[i]=='.'&&check==true) cout<<"yes"<<"\n";
+		else {
+			while(!s.empty()) s.pop();
+			cout<<"no"<<"\n";
+		}
 	}
 	return 0;
 }
