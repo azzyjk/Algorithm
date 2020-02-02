46747534107bf8ada405fa12c9cd5ea115673e8c
azzyjk <azzyjk@naver.com>
2020-02-02 14:17:08 +0000

    Fix code because 'memory over'

--- BackTracking/9663.cpp
+++ BackTracking/9663.cpp
@@ -1,72 +1,67 @@
 #include<iostream>
 #define MAX 14
 using namespace std;
-void Print(int ar[][MAX]);
 
-int N, res=0;
-
-void copy(int ar[][MAX], int cp[][MAX]){
-	for(int i=0; i<N; i++){
-		for(int j=0; j<N; j++){
-			cp[i][j]=ar[i][j];
-		}
+int ar[MAX][MAX];
+int res;
+				 /*{{0, 0, 1, 0}
+				 ,{1, 0, 0, 0}
+				 ,{0, 0, 0, 0}
+				 ,{0, 0, 0, 0}
+				 };*/
+int Facto(int n){
+	int sum=1;
+	for(int i=1; i<=n; i++){
+		sum*=i;
 	}
+	return sum;
 }
 
-void check(int x, int y, int cnt, int ar[][MAX]){
-	int cp[MAX][MAX]={'\0', };
-	copy(ar, cp);
-	if(cnt<4){
-		for(int i=0; i<N; i++){
-			cp[y][i]=1;
-			cp[i][x]=1;
-			if(x+i<N){
-				if(y-i>=0) cp[y-i][x+i]=1;
-				if(y+i<N) cp[y+i][x+i]=1;
-			}
-			if(x-i>=0){
-				if(y-i>=0) cp[y-i][x-i]=1;
-				if(y+i<N) cp[y+i][x-i]=1;
-			}
+bool check(int i, int j, int N){
+	//cout<<"i, j : "<<i<<", "<<j<<endl;
+	for(int k=0; k<N; k++){
+		//cout<<"ar[i][k] : "<<ar[i][k]<<endl;
+		if(ar[i][k]==1) return false;
+		else if(ar[k][j]==1) return false;
+		if(i-k>=0){
+			if(j-k>=0&&ar[i-k][j-k]==1) return false;
+			if(j+k<N&&ar[i-k][j+k]==1) return false;
 		}
-		for(int i=0; i<N; i++){
-			for(int j=0; j<N; j++){
-				if(cp[i][j]==1) continue;
+		if(i+k<N){
+			if(j-k>=0&&ar[i+k][j-k]==1) return false;
+			if(j+k<N&&ar[i+k][j+k]==1) return false;
+		}
+	}
+	return true;
+}
+
+void exer(int N, int cnt){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			//cout<<"level : "<<cnt<<" - "<<check(i,j,N)<<"|| i, j : "<<i<<", "<<j<<endl;
+			if(ar[i][j]==1) continue;
+			else if(check(i, j, N)==true) {
+				if(cnt<N){
+					//cout<<"level : "<<cnt<<" || [i][j] : "<<i<<", "<<j<<endl;
+					ar[i][j]=1;
+					exer(N, cnt+1);
+					ar[i][j]=0;
+				}
 				else{
-					check(j, i, cnt+=1, cp);
-				}
-			}
-		}
-	}
-	else{
-		for(int i=0; i<N; i++){
-			for(int j=0; j<N; j++){
-				if(cp[i][j]==1) continue;
-				else {
-					cout<<"i, j : "<<i<<", "<<j<<"\n";
-					Print(cp);
+					//cout<<"checked i, j : "<<i<<", "<<j<<endl;
 					res++;
-				}
+				}		
 			}
 		}
-	}
-}
-
-void Print(int ar[][MAX]){
-	for(int i=0; i<N; i++){
-		for(int j=0; j<N; j++){
-			cout<<ar[i][j]<<" ";
-		}
-		cout<<endl;
 	}
 }
 
 int main(){
+	int N;
 	cin>>N;
-	int ar[MAX][MAX]={'\0', };
-	check(0, 0, 1, ar);
-	Print(ar);
 	
-	cout<<res<<"\n";
+	exer(N, 1);
+	cout<<res/Facto(N)<<endl;
+	
 	return 0;
 }
