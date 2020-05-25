30315fe8cf23f7d9f500164c9ca4460b705b0982
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-05-26 04:34:35 +0900

    Update setting

--- BackTracking/1.cpp
+++ BackTracking/1.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #define MAX 9
 
 using namespace std;
@@ -8,35 +8,43 @@ int N, M;
 vector<int> v;
 bool check[MAX];
 
-void Print(){
-	for(vector<int>::size_type i=0; i<v.size(); i++){
-		cout<<v[i]<<" ";
+void Print()
+{
+	for (vector<int>::size_type i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
 	}
-	cout<<"\n";
+	cout << "\n";
 }
 
-void backTK(){
-	if(v.size()==M) Print();
-	else {
-		for(int i=1; i<=N; i++){
-			if(check[i]==true) continue;
+void backTK()
+{
+	if (v.size() == M)
+		Print();
+	else
+	{
+		for (int i = 1; i <= N; i++)
+		{
+			if (check[i] == true)
+				continue;
 			v.push_back(i);
-			check[i]=true;
+			check[i] = true;
 			backTK();
-			check[i]=false;
+			check[i] = false;
 			v.pop_back();
 		}
 	}
 }
 
-int main(){
+int main()
+{
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	
-	cin>>N>>M;
-	
+
+	cin >> N >> M;
+
 	backTK();
-	
+
 	return 0;
 }
