3d9146322830e57101614e5abef44f25a60a6aad
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-08-30 01:01:50 +0900

    Fix 2740(Divide conquer)

--- DivideConquer/2740.cpp
+++ DivideConquer/2740.cpp
@@ -15,37 +15,27 @@ int main(){
         }
     }
     cin >> M >> K;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
+    for(int i=0; i<M; i++){
+        for(int j=0; j<K; j++){
             cin >> temp;
             v2[i].push_back(temp);
         }
     }
 
     for(int i=0; i<N; i++){
-        resTemp =0;
-        for(int j=0; j<M; j++){
-            cout<< "v1 : " << v1[i][j] << " v2 : " << v2[j][i]<<endl;
-            resTemp += v1[i][j] * v2[j][i];
+        for(int k=0; k<K; k++){
+            resTemp =0;
+                for(int j=0; j<M; j++) resTemp += v1[i][j] * v2[j][k];
+            res[i].push_back(resTemp);
         }
-        cout << "res temp : " << resTemp<< endl;
-        res[i].push_back(resTemp);
+        
     }
 
-    cout << "결과" << endl;
-    for(int i=0; i<M; i++){
+    for(int i=0; i<N; i++){
         for(int j=0; j<K; j++){
             cout << res[i][j] << " ";
         }
         cout << endl;
     }
-    
-    // for(int i=0; i<N; i++){
-    //     for(int j=0; j< M; j++){
-    //         cout << v[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
     return 0;
 }
