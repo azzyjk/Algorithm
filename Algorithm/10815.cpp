d46275d2b5c9be7f3deef99599eed9d6208bd284
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-01-10 21:52:37 +0900

    Solve 10815

---
+++ Algorithm/10815.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> v;
+
+int search(int num){
+    int start=0, end=v.size()-1, mid;
+
+    while(start<=end){
+        mid = (start+end)/2;
+        if(v[mid] == num) break;
+        if(v[mid]>num) end = mid -1;
+        else if(v[mid]<num) start = mid + 1;
+        
+    }
+    if(v[mid] == num) return 1;
+    return 0;
+}
+
+int main(){
+    int N, M, tmp;
+    vector<int> find;
+
+    cin >> N;
+
+    for(int i=0; i<N; i++){
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+
+    cin >> M;
+
+    for(int i=0; i<M; i++){
+        cin >> tmp;
+        find.push_back(tmp);
+    }
+
+    sort(v.begin(), v.end());
+    
+    for(int i=0; i<find.size(); i++){
+        cout << search(find[i]) << " ";
+    }
+
+    return 0;
+}
